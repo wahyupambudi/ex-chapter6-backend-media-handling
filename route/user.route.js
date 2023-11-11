c75fb167b40f962e31f0ba8d317956549a3a3747
wahyupambudi <wahyupambudi823@gmail.com>
2023-11-11 10:53:16 +0700

    update router for imagekit

--- route/user.route.js
+++ route/user.route.js
@@ -11,10 +11,9 @@ const {
 const storage = require("../lib/multer");
 const multer = require("multer")();
 
-// router.post("/images", multer.single("images"), imageKitUpload);
-
 router.post("/image-single", storage.Image.single("images"), MediaProcessingImage);
 router.post("/images", storage.Image.array("images", 3), MediaProcessingImages);
+router.post("/imagekit", multer.single("images"), imageKitUpload);
 router.post("/videos", storage.Video.single("videos"), MediaProcessingVideo);
 router.post("/files", storage.File.single("f iles"), MediaProcessingFile);
 router.post("/qr-code", generateQr);
